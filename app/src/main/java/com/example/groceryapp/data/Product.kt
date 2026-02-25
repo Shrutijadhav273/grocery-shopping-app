7d33a625e79ad00d3aeec2b7e5c56414b0ddff81
Shrutijadhav273 <shrutijadhav969@gmail.com>
2026-02-25 15:02:14 +0530

    updated

--- app/src/main/java/com/example/groceryapp/data/Product.kt
+++ app/src/main/java/com/example/groceryapp/data/Product.kt
@@ -1,4 +1,9 @@
+
 package com.example.groceryapp.data
 
-class Product {
-}
+data class Product(
+    val id: String = "",
+    val name: String = "",
+    val price: String = "",
+    val image: String = ""
+)
--- app/src/main/java/com/example/groceryapp/ui/theme/Color.kt
+++ app/src/main/java/com/example/groceryapp/ui/theme/Color.kt
@@ -2,10 +2,7 @@ package com.example.groceryapp.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
-
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+val PrimaryGreen = Color(0xFF2ECC71)
+val LightGreen = Color(0xFFE8F8F1)
+val DarkGreen = Color(0xFF27AE60)
+val White = Color(0xFFFFFFFF)
