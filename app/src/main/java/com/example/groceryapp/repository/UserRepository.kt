6370047c442486acf4b410c6bf83e76498ad19db
Shrutijadhav273 <shrutijadhav969@gmail.com>
2026-02-25 13:38:07 +0530

    chnages repository file

--- app/src/main/java/com/example/groceryapp/repository/UserRepository.kt
+++ app/src/main/java/com/example/groceryapp/repository/UserRepository.kt
@@ -1,4 +1,11 @@
 package com.example.groceryapp.repository
 
-class UserRepository {
+import com.example.groceryapp.data.User
+import com.example.groceryapp.data.UserDao
+
+class UserRepository(private val dao: UserDao) {
+
+    suspend fun login(email: String, password: String) = dao.login(email, password)
+    suspend fun register(user: User) = dao.register(user)
+    suspend fun getUserByEmail(email: String) = dao.getUserByEmail(email)
 }
