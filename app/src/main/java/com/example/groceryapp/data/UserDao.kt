68bd4113ac171d9ab14f8d0768a1ff520c339206
Shrutijadhav273 <shrutijadhav969@gmail.com>
2026-02-25 13:36:21 +0530

    user files cahnges

--- app/src/main/java/com/example/groceryapp/data/UserDao.kt
+++ app/src/main/java/com/example/groceryapp/data/UserDao.kt
@@ -1,4 +1,16 @@
 package com.example.groceryapp.data
 
-class UserDao {
+import androidx.room.*
+
+@Dao
+interface UserDao {
+
+    @Query("SELECT * FROM users WHERE email = :email AND password = :password")
+    suspend fun login(email: String, password: String): User?
+
+    @Insert
+    suspend fun register(user: User)
+
+    @Query("SELECT * FROM users WHERE email = :email")
+    suspend fun getUserByEmail(email: String): User?
 }
